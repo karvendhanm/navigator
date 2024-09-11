b6c886b7ef55dd4132892e695f1a799210e10739
karvendhanm <karvsmech@gmail.com>
2024-09-11 09:36:33 +0530

    defining a multi-layer perceptron object in pytorch

--- pytorch_tutorials/demo.py
+++ pytorch_tutorials/demo.py
@@ -1,19 +1,33 @@
 import torch
 import torch.nn as nn
 
-import pprint
-pp = pprint.PrettyPrinter()
 
-list_of_lists = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-print(list_of_lists)
+class MultiLayerPerceptron(nn.Module):
 
-# initializing a pytorch tensor
-data = torch.tensor([
-    [0, 1],
-    [2, 3],
-    [4, 5]
-])
-print(data)
+    def __init__(self, input_size, hidden_size):
+
+        super(MultiLayerPerceptron, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+
+        self.model = nn.Sequential(
+            nn.Linear(self.input_size, self.hidden_size),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size, self.input_size),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        output = self.model(x)
+        return output
+
+
+input = torch.randn(2, 5)
+model = MultiLayerPerceptron(5, 3)
+model(input)
+
+
+
+
+
