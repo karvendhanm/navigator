4f774791ea40c0299a39a78c78c11c009577cafc
karvendhanm <karvsmech@gmail.com>
2024-10-17 10:00:42 +0530

    create auto_models2.py

---
+++ hugging_face/auto_models2.py
@@ -0,0 +1,23 @@
+import torch
+
+from transformers import (AutoTokenizer,
+                          AutoModelForSequenceClassification,
+                          DistilBertForSequenceClassification)
+
+check_point = 'siebert/sentiment-roberta-large-english'
+model_auto = AutoModelForSequenceClassification.from_pretrained(check_point)
+# model_bert = DistilBertForSequenceClassification.from_pretrained(check_point, num_labels = 2)
+
+tokenizer = AutoTokenizer.from_pretrained(check_point)
+
+input_str = "the movie was good"
+tokenized_inputs = tokenizer(input_str, return_tensors='pt')
+tokenized_inputs
+
+model_outputs = model_auto(**tokenized_inputs)
+model_outputs
+
+print(f"Distribution over labels: {torch.softmax(model_outputs.logits, dim=1)}")
+
+labels = ['NEGATIVE', 'POSITIVE']
+print(labels[torch.argmax(model_outputs.logits)])
