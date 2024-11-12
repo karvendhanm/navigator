86ef691d53561c1182cc589e56172d790e13fa19
karvendhanm <karvsmech@gmail.com>
2024-11-12 06:05:10 +0530

    transformer applications overview

---
+++ NLPT/transformer_applications/transformer_applications.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+
+text = """Dear Amazon, last week I ordered an Optimus Prime action figure \
+from your online store in Germany. Unfortunately, when I opened the package, \
+I discovered to my horror that I had been sent an action figure of Megatron \
+instead! As a lifelong enemy of the Decepticons, I hope you can understand my \
+dilemma. To resolve the issue, I demand an exchange of Megatron for the \
+Optimus Prime figure I ordered. Enclosed are copies of my records concerning \
+this purchase. I expect to hear from you soon. Sincerely, Bumblebee."""
+
+translator = pipeline('translation_en_to_de', model='Helsinki-NLP/opus-mt-en-de')
+outputs = translator(text, max_length=100, clean_up_tokenization_spaces=True)
+print(outputs[0]['translation_text'])
