91b5c551ac5898734e2c4061e1d306552632ca09
karvendhanm <karvsmech@gmail.com>
2024-12-04 06:10:49 +0530

    lower triangular matrix for masked multi-head self-attention

---
+++ decoder_masked_multi_head_self_attention_mechanism.py
@@ -0,0 +1,23 @@
+from torch import nn
+from transformers import AutoTokenizer, AutoConfig
+
+import math
+import torch
+
+text = 'time flies like an arrow'
+model_ckpt = 'bert-base-uncased'
+config = AutoConfig.from_pretrained(model_ckpt)
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+
+embeddings = nn.Embedding(config.vocab_size, config.hidden_size)
+
+inputs = tokenizer(text, add_special_tokens=False, padding=True, truncation=True, return_tensors='pt')
+token_embedding = embeddings(inputs.input_ids)
+
+query = key = value = token_embedding
+dim_k = query.size(-1)
+attention_scores = torch.bmm(query, key.transpose(1, 2)) / math.sqrt(dim_k)
+
+seq_len = inputs.input_ids.size(-1)
+mask = torch.tril(torch.ones(seq_len, seq_len)).unsqueeze(0)
+attention_scores.masked_fill(mask == 0, float("-inf"))
