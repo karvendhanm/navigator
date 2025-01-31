ba3d0377f302ef53894e1873b2e764e38f42f405
karvendhanm <karvsmech@gmail.com>
2025-02-01 05:24:35 +0530

    determining the F1 of the NER fine-tuned  model in various languages

--- NLPT/NER/cross_lingual_transfer.py
+++ NLPT/NER/cross_lingual_transfer.py
@@ -172,6 +172,43 @@ df = tag_text(xlmr_tokenizer, trainer.model, tags, text_fr)
 print(df)
 
 
+def tokenize_and_align_labels(examples):
+    tokenized_inputs = xlmr_tokenizer(examples['tokens'],
+                                     truncation=True,
+                                     is_split_into_words=True)
+    labels=[]
+    for idx, label in enumerate(examples['ner_tags']):
+        word_ids = tokenized_inputs.word_ids(batch_index=idx)
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:
+            if word_idx is None or word_idx == previous_word_idx:
+                label_ids.append(-100)
+            else:
+                label_ids.append(label[word_idx])
+            previous_word_idx = word_idx
+        labels.append(label_ids)
+    tokenized_inputs['labels'] = labels
+    return tokenized_inputs
+
+
+def encode_panx_dataset(corpus):
+    return corpus.map(tokenize_and_align_labels,
+                      batched=True,
+                      remove_columns=['langs', 'ner_tags', 'tokens'])
+
+def evaluate_lang_performance(lang, trainer):
+    panx_ds = encode_panx_dataset(panx_ch[lang])
+    return get_f1_score(trainer, panx_ds['test'])
+
+
+print(evaluate_lang_performance('fr', trainer))
+print(evaluate_lang_performance('it', trainer))
+print(evaluate_lang_performance('en', trainer))
+
+
+
+
 
 
 
