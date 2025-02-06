8278d2466f1253071983b635af4e40d97c4f5a44
karvendhanm <karvsmech@gmail.com>
2025-02-06 07:55:02 +0530

    sequence log probability

--- NLPT/text_generation/greedy_search_decoding.py
+++ NLPT/text_generation/greedy_search_decoding.py
@@ -2,6 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 import pandas as pd
 import torch
+import torch.nn.functional as F
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -48,5 +49,56 @@ valley, in the Andes Mountains. Even more surprising to the \
 researchers was the fact that the unicorns spoke perfect English.\n\n
 """
 input_ids = tokenizer(input_txt, return_tensors='pt')['input_ids'].to(device)
-output = model.generate(input_ids, max_length=max_length, do_sample=False)
-print(tokenizer.decode(output[0]))
+output_greedy = model.generate(input_ids, max_length=max_length, do_sample=False)
+print(tokenizer.decode(output_greedy[0]))
+
+
+def log_probs_from_logits(logits, greedy_output_tokens):
+    logp = F.log_softmax(logits, dim=-1)
+    logp_label = torch.gather(input=logp, dim=2, index=greedy_output_tokens.unsqueeze(2)).squeeze(-1)
+    return logp_label
+
+
+def sequence_logprob(model, greedy_output_tokens, input_len=0):
+    with torch.no_grad():
+        output = model(greedy_output_tokens)
+        log_probs = log_probs_from_logits(output.logits[:, :-1, :], greedy_output_tokens[:, 1:])
+        seq_log_prob = torch.sum(log_probs[:, input_len:])
+    return seq_log_prob.cpu().numpy()
+
+
+logp = sequence_logprob(model, output_greedy, input_len=len(input_ids[0]))
+print(tokenizer.decode(output_greedy[0]))
+print(f"\nlog-prob: {logp:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
