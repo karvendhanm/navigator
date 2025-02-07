c9fd5af52270100465d58a6c1688f7d14b8fa641
karvendhanm <karvsmech@gmail.com>
2025-02-07 07:06:05 +0530

    greedy_search_decoding

--- NLPT/text_generation/greedy_search_decoding.py
+++ NLPT/text_generation/greedy_search_decoding.py
@@ -13,6 +13,14 @@ model = AutoModelForCausalLM.from_pretrained(model_checkpoint).to(device)
 # greedy search decoding by custom method
 input_txt = 'Transformers are the'
 input_ids = tokenizer(input_txt, return_tensors='pt')['input_ids'].to(device)
+
+# using generate method
+output = model.generate(input_ids, max_new_tokens=8)
+output_str = tokenizer.decode(output[0])
+print(f'the output created by the causalML generate method: {output_str}')
+
+# CausalLM models work on a loop. They generate a token and use it to predict the next token.
+# recreating the generate method.
 iterations = []
 n_steps = 8
 choices_per_step = 5
@@ -20,28 +28,24 @@ choices_per_step = 5
 with torch.no_grad():
     for _ in range(n_steps):
         iteration = dict()
-        iteration['input'] = tokenizer.decode(input_ids[0])
-
-        output = model(input_ids=input_ids)
+        iteration['Input'] = tokenizer.decode(input_ids[0])
+        output = model(input_ids)
         next_token_logits = output.logits[0, -1, :]
         next_token_probs = torch.softmax(next_token_logits, dim=-1)
         sorted_ids = torch.argsort(next_token_probs, dim=-1, descending=True)
 
         for choice_idx in range(choices_per_step):
             token_id = sorted_ids[choice_idx]
-            token_probs = next_token_probs[token_id]
-            token_choice = f'{tokenizer.decode(token_id)} ({token_probs * 100:.2f}%)'
-            iteration[f'Choice {choice_idx + 1}'] = token_choice
-        input_ids = torch.cat([input_ids, sorted_ids[None, 0, None]], dim=-1)
+            token_prob = next_token_probs[token_id]
+            iteration[f'Choice {choice_idx + 1}'] = f'{tokenizer.decode(token_id)} ({token_prob * 100:.2f}%)'
+        input_ids = torch.cat((input_ids, sorted_ids[None, 0, None]), dim=-1)
         iterations.append(iteration)
-
 pd.DataFrame(iterations)
 
-# greedy search decoding using transformers inbuilt generate method
-input_ids = tokenizer(input_txt, return_tensors='pt')['input_ids'].to(device)
-output = model.generate(input_ids, max_new_tokens=n_steps, do_sample=False)
-print(tokenizer.decode(output[0]))
 
+# decoding techniques
+# comparing log probability of a sequence using
+# greedy decoding and beam search decoding
 max_length = 128
 input_txt = """In a shocking finding, scientist discovered \
 a herd of unicorns living in a remote, previously unexplored \
@@ -49,27 +53,11 @@ valley, in the Andes Mountains. Even more surprising to the \
 researchers was the fact that the unicorns spoke perfect English.\n\n
 """
 input_ids = tokenizer(input_txt, return_tensors='pt')['input_ids'].to(device)
-output_greedy = model.generate(input_ids, max_length=max_length, do_sample=False)
-print(tokenizer.decode(output_greedy[0]))
+output = model.generate(input_ids, max_length=max_length)
+print(f'the generated text is: {tokenizer.decode(output[0])}')
 
 
-def log_probs_from_logits(logits, greedy_output_tokens):
-    logp = F.log_softmax(logits, dim=-1)
-    logp_label = torch.gather(input=logp, dim=2, index=greedy_output_tokens.unsqueeze(2)).squeeze(-1)
-    return logp_label
 
-
-def sequence_logprob(model, greedy_output_tokens, input_len=0):
-    with torch.no_grad():
-        output = model(greedy_output_tokens)
-        log_probs = log_probs_from_logits(output.logits[:, :-1, :], greedy_output_tokens[:, 1:])
-        seq_log_prob = torch.sum(log_probs[:, input_len:])
-    return seq_log_prob.cpu().numpy()
-
-
-logp = sequence_logprob(model, output_greedy, input_len=len(input_ids[0]))
-print(tokenizer.decode(output_greedy[0]))
-print(f"\nlog-prob: {logp:.2f}")
 
 
 
