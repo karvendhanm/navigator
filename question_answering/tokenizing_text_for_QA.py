d9b3c781434562083d0be54722279f0e196b0d67
karvendhanm <karvsmech@gmail.com>
2025-03-13 06:20:20 +0530

    visualizing question answering problem

--- question_answering/tokenizing_text_for_QA.py
+++ question_answering/tokenizing_text_for_QA.py
@@ -1,33 +1,41 @@
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+
+# question answering.
+question = "how much music can this hold?"
+context = """An MP3 is about 1 MB/minute, so about 6000 hours depending on file size."""
 
 model_ckpt = "deepset/minilm-uncased-squad2"
 tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
 model = AutoModelForQuestionAnswering.from_pretrained(model_ckpt).to(device)
 
-
-question = "how much music can this hold"
-context = """An MP3 is about 1 MB/minute, so about 6000 hours depending on file size"""
-
 inputs = tokenizer(question, context, return_tensors='pt').to(device)
-print(tokenizer.decode(inputs['input_ids'][0]))
-
 with torch.no_grad():
-    outputs = model(**inputs)
-print(outputs)
+    output = model(**inputs)
 
-print(f'the shape of the input id is: {inputs.input_ids.size()}')
-print(f'start logits shape: {outputs.start_logits.size()}')
-print(f'end logits shape: {outputs.end_logits.size()}')
-
-start_idx = torch.argmax(outputs.start_logits).cpu().numpy()
-end_idx = torch.argmax(outputs.end_logits).cpu().numpy()
-answer_span = inputs['input_ids'][0][start_idx:end_idx+1]
+start_idx = torch.argmax(output.start_logits)
+end_idx = torch.argmax(output.end_logits)
+answer_span = inputs.input_ids[0][start_idx:end_idx + 1]
 answer = tokenizer.decode(answer_span)
-print(f'Question:{question}')
-print(f'Answer:{answer}')
 
+# visualization
+tokens = tokenizer.convert_ids_to_tokens(inputs.input_ids[0])
+token_idx = range(len(tokens))
+
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
+s_scores = output.start_logits.cpu().numpy().flatten()
+s_colors = ['C1' if score == np.max(s_scores) else 'C0' for score in s_scores]
+e_scores = output.end_logits.cpu().numpy().flatten()
+e_colors = ['C1' if score == np.max(e_scores) else 'C0' for score in e_scores]
+ax1.bar(x=token_idx, height=s_scores, color=s_colors)
+ax1.set_ylabel('Start Scores')
+ax2.bar(x=token_idx, height=e_scores, color=e_colors)
+ax2.set_ylabel('End Scores')
+plt.xticks(token_idx, tokens, rotation='vertical')
+plt.show()
 
